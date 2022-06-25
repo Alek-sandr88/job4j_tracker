c5c4f5aa01348d7cc7bbe24c01c2d46b05be7748
Aleksander <al.ast8873@gmail.com>
2022-06-25 14:57:46 +0400

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/tracker/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load - another.load;
+    }
+
+    public static void main(String[] args) {
+        Battery battery = new Battery(10);
+        battery.exchange(battery);
+    }
+}
