4f090ccc17ad27e7af9d61aafe734d4ad814bf42
Aleksander <al.ast8873@gmail.com>
2022-06-25 14:30:32 +0400

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void swoInfo() {
+        System.out.println(this.active + " " + this.message + " " + this.status);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error1 = new Error(true, 1, "Объект1");
+        Error error2 = new Error(false, 2, "Объект2");
+        Error error3 = new Error(true, 3, "Объект3");
+        error.swoInfo();
+        error1.swoInfo();
+        error2.swoInfo();
+        error3.swoInfo();
+    }
+}
