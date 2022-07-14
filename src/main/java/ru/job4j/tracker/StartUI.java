ef50bd06f9014cbc9a40f66e6cebe614c0a71492
Aleksander <al.ast8873@gmail.com>
2022-07-14 14:42:50 +0400

    2.2. Реализация класса StartUI. Вывод меню [#500743 #321400]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -27,7 +27,6 @@ public class StartUI {
         System.out.println("Menu:");
         for (int i = 0; i < menu.length; i++) {
             System.out.println(i + ". " + menu[i]);
-            boolean run = true;
         }
 
     }
