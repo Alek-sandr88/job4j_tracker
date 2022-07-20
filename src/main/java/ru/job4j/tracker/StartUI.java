630f7d82043ef645a3bd970b14e7c703853f9c0a
Aleksander <al.ast8873@gmail.com>
2022-07-20 13:14:43 +0400

    2.8. Реализация класса StartUI. Вывод заявок по имени [#500749]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,14 +12,14 @@ public class StartUI {
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
             if (select == 0) {
-                System.out.println("=== Create a new Item ===");
-                System.out.println("Enter name: ");
+                System.out.println("=== Создать новый элемент ===");
+                System.out.println("Введите имя: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
             } else if (select == 1) {
-                System.out.println("=== Show all items ===");
+                System.out.println("=== Показать все элементы ===");
                 Item[] items = tracker.findAll();
                 if (items.length > 0) {
                     for (Item item : items) {
@@ -29,10 +29,10 @@ public class StartUI {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
             } else if (select == 2) {
-                System.out.println("=== Edit item ===");
-                System.out.print("Enter id: ");
+                System.out.println("=== Изменить элемент ===");
+                System.out.print("Введите идентификатор: ");
                 int id = Integer.parseInt(scanner.nextLine());
-                System.out.print("Enter name: ");
+                System.out.print("Введите имя: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 if (tracker.replace(id, item)) {
@@ -41,8 +41,8 @@ public class StartUI {
                     System.out.println("Ошибка замены заявки.");
                 }
             } else if (select == 3) {
-                System.out.println("=== Delete item ===");
-                System.out.print("Enter id: ");
+                System.out.println("=== Удалить пункт ===");
+                System.out.print("Введите идентификатор: ");
                 int id = Integer.parseInt(scanner.nextLine());
                 if (tracker.delete(id)) {
                     System.out.println("Заявка удалена успешно.");
@@ -50,8 +50,8 @@ public class StartUI {
                     System.out.println("Ошибка удаления заявки.");
                 }
             } else if (select == 4) {
-                System.out.println("=== Find item by id ===");
-                System.out.print("Enter id: ");
+                System.out.println("=== Найти элемент по идентификатору ===");
+                System.out.print("Введите идентификатор: ");
                 int id = Integer.parseInt(scanner.nextLine());
                 Item item = tracker.findById(id);
                 if (item != null) {
@@ -59,17 +59,30 @@ public class StartUI {
                 } else {
                     System.out.println("Заявка с введенным id: " + id + " не найдена.");
                 }
+            } else if (select == 5) {
+                System.out.println("=== Поиск по названию ===");
+                System.out.print("Введите имя: ");
+                String name = scanner.nextLine();
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Заявки с именем: " + name + " не найдены.");
+                }
             } else if (select == 6) {
                 run = false;
             }
         }
     }
 
+
     private void showMenu() {
         String[] menu = {
-                "Add new Item", "Show all items", "Edit item",
-                "Delete item", "Find item by id", "Find items by name",
-                "Exit Program"
+                "Добавить новый элемент", "Показать все элементы", "Изменить элемент",
+                "Удалить пункт", "Найти элемент по идентификатору", "Поиск по названию",
+                "Выход из программы"
         };
         System.out.println("Menu:");
         for (int i = 0; i < menu.length; i++) {
